91c6b320e1b843d31bf4ee959b6a017c15064782
eze <106458263+eze2013@users.noreply.github.com>
2022-08-22 10:10:23 -0300

    si

--- mi_app/models.py
+++ mi_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Curso (models.Model):
+
+    nombre = models.CharField(max_length=40)  # como en base de datos pide un tipo de dato y su longitud
+    camada = models.IntegerField() # este es para campos numericos enteros
+
+
